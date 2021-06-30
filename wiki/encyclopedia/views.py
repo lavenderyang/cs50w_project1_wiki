66dcc9d2d137b10ef3fc8020a6648593f2cb621c
Susan Yang <YangS@moe.govt.nz>
2021-06-30 13:35:03 +1200

    Add Entry Page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,21 @@
+from django.http import HttpResponse, Http404
+
 from django.shortcuts import render
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, TITLE):
+    entry = util.get_entry(TITLE)
+    if not entry:  
+        return render(request, "encyclopedia/error.html", {"message": "You requested page was not found."})
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry, 
+        "title": TITLE
+
     })
 
